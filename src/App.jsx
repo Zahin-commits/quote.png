588ba5907aab79d19c99eee6cca206fe0c0263ec
Zahin-commits <zahinhosen@gmail.com>
2024-03-20 00:07:47 +0600

    added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import { useState, useRef} from 'react';
 import html2canvas from 'html2canvas';
 
 function App() {
+
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
   const [imageUrl, setImageUrl] = useState('');
+
+  const inputRef = useRef('');
+
+  const handleClick = ()=>{
+    inputRef.current.click();
+   }
 
   const fetchQuote = () => {
     fetch('https://dummyjson.com/quotes/random')
@@ -25,6 +32,7 @@ function App() {
     reader.readAsDataURL(file);
   };
 
+
   const handleDownload = () => {
     html2canvas(document.getElementById('quote-container')).then(canvas => {
       const url = canvas.toDataURL();
@@ -41,22 +49,32 @@ function App() {
 
   return (
     <>
-    <div>
-      <button onClick={fetchQuote}>Get Quote</button>
-      <input type="file" accept="image/*" onChange={handleImageUpload} />
+    <div id='main'>
+      <div className="input_btn">
+       <button className='btn get_quote' onClick={fetchQuote}>Get Quote</button>
+        <div className="btn get_img" onClick={handleClick}>
+          Choose file 
+         <input  type="file" accept="image/*" 
+          ref={inputRef}
+          hidden
+          onChange={handleImageUpload} />
+        </div>
+      </div>
+
       {imageUrl && (
-        <div>
+        
          <div id="quote-container" style={{ position: 'relative' }}>
           <img className='uploaded_img' src={imageUrl} alt="Uploaded" />
+          {/* <img className='uploaded_img' src={"https://random-image-pepebigotes.vercel.app/api/random-image"} alt="Uploaded" /> */}
          
           <div className='quote_box'>
             <p className='quote'>{quote}</p>
             <p className='author'>- {author}</p>
           </div>
          </div>
-          <button onClick={handleDownload}>Download</button>
-        </div>
+     
       )}
+      <button className='btn download' onClick={handleDownload}>Download</button>
     </div>
     </>
   );
@@ -134,3 +152,4 @@ export default App */
 
 
 //https://dummyjson.com/quotes/random
+//https://random-image-pepebigotes.vercel.app/api/random-image
